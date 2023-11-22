cf281de465cf0401a06c053f57ddd98b58dd36d4
Christian Briones <christianbriones@Christians-MacBook-Air-2.local>
2023-11-22 17:22:46 -0600

    started adding the questions and correct answer part

---
+++ asset/questions.js
@@ -0,0 +1,28 @@
+var questions = [
+  {
+    title:"What is your name?",
+    choices:["Jill", "Sam", "Faran", "Simba"],
+    correctAnswer: "Faran"
+  },
+  {
+    title:"What is your name1?",
+    choices:["Jill", "Sam", "Faran", "Simba"],
+    correctAnswer: "Faran"
+  },
+  {
+    title:"What is your name2?",
+    choices:["Jill", "Sam", "Faran", "Simba"],
+    correctAnswer: "Faran"
+  },
+  {
+    title:"What is your name3?",
+    choices:["Jill", "Sam", "Faran", "Simba"],
+    correctAnswer: "Faran"
+  },
+  {
+    title:"What is your name4?",
+    choices:["Jill", "Sam", "Faran", "Simba"],
+    correctAnswer: "Faran"
+  },
+  
+ ]
--- asset/script.js
+++ asset/script.js
@@ -3,11 +3,16 @@ var timerEl = document.querySelector("#timer");
 var startBtn = document.querySelector('#start');
 var startScreenEl = document.querySelector('#start-screen')
 var quizScreenEl = document.querySelector('#quiz-screen')
+var questionEl = document.querySelector("#question")
+var choicesEl = document.querySelector("#choices")
+
 
 
 //functional variables
 var time = 60;
 var timeInterval;
+var qIndex = 0;
+
 
 
 function startGame(a) {
@@ -21,10 +26,42 @@ function startGame(a) {
     time--;
     timerEl.textContent = time;
   }, 1000)
+
+  renderQ()
+  
+    //TTimer triggers a question 
+    //create text box for answer
+    //log the right answer
+
+    //repeat ^^^ 4-5 times
+
 }
 
-function starttimer() {
+function renderQ() {
+  var currentQ = questions[qIndex]
 
+  questionEl.textContent = currentQ.title;
+
+  for (let i = 0; i < currentQ.choices.length; i++) {
+    const choiceBtn = document.createElement('button')
+    choiceBtn.textContent = currentQ.choices[i]
+
+    choiceBtn.onclick = checkAnswer;
+
+    choicesEl.appendChild(choiceBtn)
+  }
+}
+
+function checkAnswer(event) {
+  var userChoice = event.target.textContent
+  
+  if (userChoice === questions[qIndex].correctAnswer) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+  qIndex++;
+  renderQ()
 }
 
 startBtn.addEventListener("click", startGame);
